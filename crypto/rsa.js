317fc988760e098fa61a3d70a0e7fd54b41a4213
YYL99 <llaayy@protonmail.com>
2021-03-31 12:01:57 +0800

    rsa

--- crypto/rsa.js
+++ crypto/rsa.js
@@ -15,6 +15,20 @@ const content = "hello world!"; // 待加密的明文内容
 // 公钥加密
 const encodeData = crypto.publicEncrypt(publicKey, Buffer.from(content));
 console.log(encodeData.toString("base64"));
+console.log(encodeData);
 // 私钥解密
 const decodeData = crypto.privateDecrypt(privateKey, encodeData);
 console.log(decodeData.toString("utf8"));
+//console.log(decodeData);
+
+const a = encodeData.toString("base64");
+const d = new Buffer.from(a, 'base64');
+console.log(d);
+
+const decodeData1 = crypto.privateDecrypt(privateKey, d);
+console.log(decodeData1.toString("utf8"));
+
+const b = new Buffer.from('SmF2YVNjcmlwdA==', 'base64')
+const s = b.toString();
+console.log(s);
+
--- transmission/server3.js
+++ transmission/server3.js
@@ -1,3 +1,10 @@
+/*# 生成公钥
+openssl rsa -in privatekey.pem -pubout -out publickey.pem
+*/
+const crypto = require("crypto");
+const fs = require("fs");
+const privateKey = fs.readFileSync("./privatekey.pem");
+
 const net = require('net');
 //模块引入
 const listenPort = 8080;//监听端口
@@ -7,10 +14,22 @@ const server = net.createServer(function(socket){
     socket.remoteAddress + ':' + socket.remotePort);
   socket.setEncoding('binary');
   socket.write('Hello client!');
+  //接收到数据,对数据进行解密
   socket.on('data',function(data){
-    console.log('client send:' + data);
+    //console.log('client send:' + data);
+    if (data == 'hello server'){
+        console.log('client send:' + data);
+    }else{
+        //console.log('client send:' + data);
+        const d = new Buffer.from(data, 'base64');
+        const decodeData = crypto.privateDecrypt(privateKey, d);//解密模块
+        const jiemi = decodeData.toString("utf8");
+        //socket.write(jiemi);
+        console.log('client send:' + jiemi);
+    }
   });
   
+
  // socket.pipe(socket);
   //数据错误事件
   socket.on('error',function(exception){
